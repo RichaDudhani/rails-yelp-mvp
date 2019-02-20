3e209e3bea638564873fdc88c8ea61e38ff7cb96
Richa Dudhani <richa.dudhani@gmail.com>
2019-02-20 15:54:26 +1100

    Yelp-mvp

---
+++ db/migrate/20190220004018_change_phonenumber_in_restaurants.rb
@@ -0,0 +1,6 @@
+class ChangePhonenumberInRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :restaurants, :phonenumber, :phone_number
+    change_column :restaurants, :phone_number, :string
+  end
+end
